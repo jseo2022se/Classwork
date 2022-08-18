b127d7727168f6c59bfc0829d3331e1499a01e34
Jeonghyun Seo <jseo2022se@gmail.com>
2022-08-18 14:51:15 -0400

    Fixing errors to part A

--- js-review/review.js
+++ js-review/review.js
@@ -6,21 +6,29 @@ I. Variables & Datatypes
 
 A. Q+A
 
-1. let variable = somevalue;
-2. variable = newvalue;
+1. Using an assignment operator '='; let variable = 1;
+2. variable = 2; by assigning it a new value like this
 3. 
-    let secondValue = 2;
-    let currentValue = 1;
-    let tempValue = currentValue; // stores currentValue in tempValue
-    currentValue = secondValue; // currentValue obtains secondValue
-    secondValue = tempValue; // secondValue obtains currentValue from tempValue
+    let variable2 = variable; 
 
 4. 
-    declare = means to declare a variable
-    assign = to assing a value to a variable
+    declare = means to first name a variable
+    assign = to set a value to a variable
     define = means to provide information all the information about something (like a variable, or the body of a function or the class)
 
 5. Pseudocoding is writing or creating a rough draft of a program using non-strict programming language. One should do it because it helps
    with planning the creation of a program and also is easier to read.
 
 6. More than 60% should be spent on thinking to solve a problem vs actually typing in code. */
+
+
+// B. Strings
+
+let firstVariable = 'Hello World';
+firstVariable = 1;
+let secondVariable = firstVariable;
+secondVariable = 'Codex Astartes';
+
+// 6. the value of the firstVariable is 1
+
+let yourName
