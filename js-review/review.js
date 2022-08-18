1ad30f1881b2173b94adf067a7c3923f0d7c33d1
Jeonghyun Seo <jseo2022se@gmail.com>
2022-08-18 14:31:18 -0400

    bank class and js review

---
+++ js-review/review.js
@@ -0,0 +1,26 @@
+/* JavaScript Total  Recall
+
+
+I. Variables & Datatypes
+
+
+A. Q+A
+
+1. let variable = somevalue;
+2. variable = newvalue;
+3. 
+    let secondValue = 2;
+    let currentValue = 1;
+    let tempValue = currentValue; // stores currentValue in tempValue
+    currentValue = secondValue; // currentValue obtains secondValue
+    secondValue = tempValue; // secondValue obtains currentValue from tempValue
+
+4. 
+    declare = means to declare a variable
+    assign = to assing a value to a variable
+    define = means to provide information all the information about something (like a variable, or the body of a function or the class)
+
+5. Pseudocoding is writing or creating a rough draft of a program using non-strict programming language. One should do it because it helps
+   with planning the creation of a program and also is easier to read.
+
+6. More than 60% should be spent on thinking to solve a problem vs actually typing in code. */
