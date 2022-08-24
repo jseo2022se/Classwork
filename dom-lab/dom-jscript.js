911f7dd6ab515f9468cf483365c1ac2a42b17b21
Jeonghyun Seo <jseo2022se@gmail.com>
2022-08-24 16:02:59 -0400

    dom lab

---
+++ dom-lab/dom-jscript.js
@@ -0,0 +1,10 @@
+// alert("test")
+
+let mainEl = document.querySelector("main")
+console.log(mainEl)
+
+mainEl.style.backgroundColor = 'var(--main-bg)'
+
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>"
+
+mainEl.classList.add("flex-ctr")
